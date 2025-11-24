2f113f203eb483383be6a91480561e4fece27bbf
Carveri <carvajal.cortes.e19@gmail.com>
2025-11-24 19:16:19 -0300

    V3 aws3

--- src/db.js
+++ src/db.js
@@ -1,16 +1,46 @@
-
 import pg from "pg";
 
-process.loadEnvFile()
+
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+const secret_name = "CENTRO_SECRETO3";
+
+const client = new SecretsManagerClient({
+  region: "us-east-1",
+});
+
+let response;
+
+try {
+  response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  );
+} catch (error) {
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw error;
+}
+
+const secret = JSON.parse(response.SecretString) 
+console.log('secreto papuuu:', secret);
+// console.log(typeof(secret));
+
 
 export const pool = new pg.Pool({
-    user: process.env.USER_PG,
-    host: process.env.HOST_PG,
-    password: process.env.PASSWORD_PG,
-    database: process.env.DATABASE_PG,
-    port: process.env.PORT_PG ?? 5432
+    user: secret.username,
+    host: secret.host,
+    password: secret.password,
+    database: secret.database,
+    port: secret.port
 })
 
 pool.query('SELECT NOW()').then(result =>{
-    //console.log('resultado de la db:', result);}
+    //console.log('resultado de la db:', result);
+    
 })
--- src/index.js
+++ src/index.js
@@ -1,46 +1,18 @@
-import pg from "pg";
+import express from "express";
+import planetaRoute from "../src/routes/planeta.route.js";
+
+const app = express()
+
+app.use(express.json())
+app.use('/api', planetaRoute)
 
 
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} from "@aws-sdk/client-secrets-manager";
 
-const secret_name = "UNIVERSO_SECRETO3";
-
-const client = new SecretsManagerClient({
-  region: "us-east-1",
-});
-
-let response;
-
-try {
-  response = await client.send(
-    new GetSecretValueCommand({
-      SecretId: secret_name,
-      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-    })
-  );
-} catch (error) {
-  // For a list of exceptions thrown, see
-  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-  throw error;
-}
-
-const secret = JSON.parse(response.SecretString) 
-console.log('secreto papuuu:', secret);
-// console.log(typeof(secret));
+const PORT = 3000
 
 
-export const pool = new pg.Pool({
-    user: secret.username,
-    host: secret.host,
-    password: secret.password,
-    database: secret.database,
-    port: secret.port
-})
 
-pool.query('SELECT NOW()').then(result =>{
-    //console.log('resultado de la db:', result);
+app.listen(PORT, ()=>{
+    console.log('corriendo');
     
 })
