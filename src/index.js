d8427a496ffac8dd9590a478150b36ae4bef4e9a
Carveri <carvajal.cortes.e19@gmail.com>
2025-11-24 19:12:35 -0300

    V2 aws2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
-import express from "express";
-import planetaRoute from "../src/routes/planeta.route.js";
-
-const app = express()
-
-app.use(express.json())
-app.use('/api', planetaRoute)
+import pg from "pg";
 
 
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 
-const PORT = 3000
+const secret_name = "UNIVERSO_SECRETO3";
+
+const client = new SecretsManagerClient({
+  region: "us-east-1",
+});
+
+let response;
+
+try {
+  response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  );
+} catch (error) {
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw error;
+}
+
+const secret = JSON.parse(response.SecretString) 
+console.log('secreto papuuu:', secret);
+// console.log(typeof(secret));
 
 
+export const pool = new pg.Pool({
+    user: secret.username,
+    host: secret.host,
+    password: secret.password,
+    database: secret.database,
+    port: secret.port
+})
 
-app.listen(PORT, ()=>{
-    console.log('corriendo');
+pool.query('SELECT NOW()').then(result =>{
+    //console.log('resultado de la db:', result);
     
 })
